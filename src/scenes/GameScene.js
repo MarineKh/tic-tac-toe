45479e738f3f6c803290299c44aff40370a30501
MarineKh <admin@Marine.local>
2018-09-12 20:45:34 +0400

    changing structure

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,31 +1,24 @@
-import Phaser from "phaser";
-import {
-  SCENE_GAME
-} from "../constants/Constants";
-import {
-  gameConfig
-} from "../constants/GameConfig";
+import Phaser from 'phaser'
+import { SCENE_GAME } from '../constants/Constants'
+import { gameConfig } from '../constants/GameConfig'
 
 export default class GameScene extends Phaser.Scene {
-  constructor() {
-    super(SCENE_GAME);
-    this.clickCount = 0;
-    this.spaceSize = 10;
-    this.matrix = [];
+  constructor () {
+    super(SCENE_GAME)
+    this.clickCount = 0
+    this.spaceSize = 10
+    this.matrix = []
   }
 
-  getPlatformSize() {
-    this.platform = this.add.image(0, 0, "platform");
-    const {
-      width,
-      height
-    } = this.platform;
-    this.platform.destroy();
-    return width;
+  getPlatformSize () {
+    this.platform = this.add.image(0, 0, 'platform')
+    const { width, height } = this.platform
+    this.platform.destroy()
+    return width
   }
 
-  create() {
-    this.boardContainer = this.add.container(0, 0);
+  create () {
+    this.boardContainer = this.add.container(0, 0)
     this.matrix = []
     for (let i = 0; i < 3; ++i) {
       this.matrix.push([])
@@ -34,61 +27,53 @@ export default class GameScene extends Phaser.Scene {
 
         const platformContainer = this.add.container(
           i * (this.getPlatformSize() + this.spaceSize),
-          j * (this.getPlatformSize() + this.spaceSize)
-        );
-        const platform = this.add.image(0, 0, "platform");
+          j * (this.getPlatformSize() + this.spaceSize),
+        )
+        const platform = this.add.image(0, 0, 'platform')
         platformContainer.setInteractive(
-          new Phaser.Geom.Circle(0, 0, platform.width / 2),
-          Phaser.Geom.Circle.Contains
-        );
-        platformContainer.add(platform);
-        this.boardContainer.add(platformContainer);
-        platformContainer.once('pointerdown', () => this.drawSymbol(i, j))
+          new Phaser.Geom.Rectangle(
+            -platform.width / 2,
+            -platform.height / 2,
+            platform.width,
+            platform.height,
+          ),
+          Phaser.Geom.Rectangle.Contains,
+        )
+        platformContainer.add(platform)
+        this.boardContainer.add(platformContainer)
+        platformContainer.data = { i, j }
       }
     }
-    // this.input.on('gameobjectdown', this.drawSymbols, this)
+    this.input.on('gameobjectdown', this.drawSymbol, this)
     this.boardContainer.x =
-      (gameConfig.width - 2 * (this.getPlatformSize() + this.spaceSize)) / 2;
+      (gameConfig.width - 2 * (this.getPlatformSize() + this.spaceSize)) / 2
     this.boardContainer.y =
-      (gameConfig.height - 2 * (this.getPlatformSize() + this.spaceSize)) / 2;
+      (gameConfig.height - 2 * (this.getPlatformSize() + this.spaceSize)) / 2
   }
 
-  drawSymbol = (i, j) => {
+  drawSymbol (pointer, target) {
     let symbol
-
+    if (this.matrix[target.data.i][target.data.j]) {
+      return
+    }
     this.clickCount++
-    this.clickCount % 2 === 1 ? (symbol = this.add.image(0, 0, 'x')) : (symbol = this.add.image(0, 0, 'o'))
-    this.matrix[i][j] = symbol.texture.key
+    symbol = this.add.image(0, 0, this.clickCount % 2 === 1 ? 'x' : 'o')
+    target.add(symbol)
+    this.matrix[target.data.i][target.data.j] = symbol.texture.key
 
     this.checkWinner()
-    console.log(this.matrix);
-    // const {
-    //   width,
-    //   height
-    // } = this.text
-    // const textX = squareX + (squareSize - width) / 2 - squareSize / 2
-    // const textY = squareY + (squareSize - height) / 2 - squareSize / 2
-    // this.text.x = textX
-    // this.text.y = textY
+    console.log(this.matrix)
   }
 
-  // drawSymbols(pointer, target, i, j) {
-  //   if (target.data) {
-  //     return
-  //   }
-  //   let symbol
-  //   this.clickCount++
-  //   this.clickCount % 2 === 1 ? (symbol = this.add.image(0, 0, 'x')) : (symbol = this.add.image(0, 0, 'o'))
-
-  //   this.matrix.push(symbol.texture.key)
-  //   target.data = true
-  //   target.add(symbol)
-  // }
-  checkWinner() {
+  checkWinner () {
     let matrix = this.matrix
     // check rows
     for (let i = 0, j = 0; i <= 2; i++) {
-      if (matrix[i][j] && matrix[i][j] === matrix[i][j + 1] && matrix[i][j + 1] === matrix[i][j + 2]) {
+      if (
+        matrix[i][j] &&
+        matrix[i][j] === matrix[i][j + 1] &&
+        matrix[i][j + 1] === matrix[i][j + 2]
+      ) {
         if (matrix[i][j] === 'x') {
           console.log('x win')
         } else {
@@ -98,7 +83,11 @@ export default class GameScene extends Phaser.Scene {
     }
     // check columns
     for (let i = 0, j = 0; i <= 2; i++) {
-      if (matrix[j][i] && matrix[j][i] === matrix[j + 1][i] && matrix[j + 1][i] === matrix[j + 2][i]) {
+      if (
+        matrix[j][i] &&
+        matrix[j][i] === matrix[j + 1][i] &&
+        matrix[j + 1][i] === matrix[j + 2][i]
+      ) {
         if (matrix[j][i] === 'x') {
           console.log('x win')
         } else {
@@ -107,13 +96,21 @@ export default class GameScene extends Phaser.Scene {
       }
     }
     // check diagonals
-    if (matrix[0][0] && matrix[0][0] === matrix[1][1] && matrix[1][1] === matrix[2][2]) {
+    if (
+      matrix[0][0] &&
+      matrix[0][0] === matrix[1][1] &&
+      matrix[1][1] === matrix[2][2]
+    ) {
       if (matrix[0][0] === 'x') {
         console.log('x win')
       } else {
         console.log('o win')
       }
-    } else if (matrix[0][2] && matrix[0][2] === matrix[1][1] && matrix[1][1] === matrix[2][0]) {
+    } else if (
+      matrix[0][2] &&
+      matrix[0][2] === matrix[1][1] &&
+      matrix[1][1] === matrix[2][0]
+    ) {
       if (matrix[0][2] === 'x') {
         console.log('x win')
       } else {
@@ -122,5 +119,5 @@ export default class GameScene extends Phaser.Scene {
     }
   }
 
-  update() {}
+  update () {}
 }
